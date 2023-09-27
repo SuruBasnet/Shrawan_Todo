81e864097e694b118099821e6fd8729792d0df17
SuruBasnet <surubasnet824@gmail.com>
2023-09-27 07:48:09 +0545

    changes

--- base/views.py
+++ base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDo
+
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    todo_objects = ToDo.objects.all()
+    content = {'todos':todo_objects}
+    return render(request,'index.html',context=content)
+
+def create(request):
+    return render(request,'create.html')
